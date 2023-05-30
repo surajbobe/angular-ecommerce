820f90cb41ad3916a7dc0f72fc1beb01d359940f
Suraj Bobe <surajreddyb@gmail.com>
2023-05-30 20:49:14 +0530

    Revert "Changes for getting product data from backend"
    This reverts commit 742ad81318a6387a27b94905334c25eb4bf71135.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './components/product-list/product-list.component';
-import { HttpClientModule } from '@angular/common/http'
+
 @NgModule({
   declarations: [
-    AppComponent,
-    ProductListComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    HttpClientModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
